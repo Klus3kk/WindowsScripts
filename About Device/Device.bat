4df160c95d321c139dfd818e5a0f0ec99475e42d
Klus3kk <lukasz.bielaszewski@onet.pl>
2024-10-30 19:18:54 +0100

    Device Info

--- About Device/Device.bat
+++ About Device/Device.bat
@@ -1,15 +1,52 @@
 @echo off
-@REM Disk space info
+echo ==============================
+echo    About Device Information
+echo ==============================
+echo Timestamp: %date% %time%
+echo.
+
+@REM Disk Space Info
+echo --- Disk Space Info ---
 wmic logicaldisk get name, size, freespace
+echo.
+
 @REM IP Fetcher
+echo --- IP Information ---
 ipconfig | findstr /C:"IPv4"
 nslookup myip.opendns.com resolver1.opendns.com | findstr /C:"Address"
+echo.
+
 @REM RAM Usage
+echo --- RAM Usage ---
 systeminfo | findstr /C:"Total Physical Memory" /C:"Available Physical Memory"
-@REM CPU/Memory Info
+echo.
+
+@REM CPU and Memory Info
+echo --- CPU and Memory Info ---
 wmic cpu get name, maxclockspeed
 wmic memorychip get capacity, speed
+echo.
+
 @REM OS Info
+echo --- Operating System Info ---
 systeminfo | findstr /B /C:"OS Name" /C:"OS Version"
+echo.
+
 @REM User Info
+echo --- User and Computer Info ---
+echo User: %username%
+echo Computer: %computername%
+echo.
+
+@REM Battery Status
+echo --- Battery Status ---
+powercfg /batteryreport
+echo Battery report saved. Opening...
+start battery-report.html
+echo.
+
+@REM Wait for user to close
+echo ==============================
+echo         End of Report
+echo ==============================
 pause
